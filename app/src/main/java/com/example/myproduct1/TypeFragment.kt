3ecf9b2acc6e97abcf73ee6c5b670735f69f919e
hp <shaxzodburxonov548@gmail.com>
2023-02-11 13:54:46 +0500

    commint

--- app/src/main/java/com/example/myproduct1/TypeFragment.kt
+++ app/src/main/java/com/example/myproduct1/TypeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myproduct1.databinding.FragmentTypeBinding
 
 class TypeFragment : Fragment() {
+    lateinit var binding: FragmentTypeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,7 +16,7 @@ class TypeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_type, container, false)
-
+        binding= FragmentTypeBinding.bind(view)
 
         return view
     }
